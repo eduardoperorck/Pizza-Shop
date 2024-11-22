4f041e48a4cc633d39edb982d3a915fbcac47c8e
Eduardo Ferraz <edupferraz@gmail.com>
2024-11-22 03:10:33 -0300

    Project structure

---
+++ pizzashop-web/src/pages/routes.tsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter } from "react-router-dom";
+import { Dashboard } from "./app/dashboard";
+import { SignIn } from "./auth/sign-in";
+import { AppLayout } from "./_layouts/app";
+import { AuthLayout } from "./_layouts/auth";
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <AppLayout/>,
+        children: [
+            {path: '/', element: <Dashboard/>},
+        ]
+    }
+
+    {
+        path: '/',
+        element: <AuthLayout/>,
+        children: [
+            {path: '/', element: <SignIn/>},
+        ]
+    }
+    
+    // {path: '/', element: <Dashboard/>},
+    // {path: '/sign-in', element: <SignIn/>},
+])
