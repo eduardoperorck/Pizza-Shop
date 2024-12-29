f6d429c8f905c51140e3458f8c348580b8003329
Eduardo Ferraz <edupferraz@gmail.com>
2024-12-29 08:24:34 -0300

    Restaurant registration page

--- pizzashop-web/src/pages/auth/sign-in.tsx
+++ pizzashop-web/src/pages/auth/sign-in.tsx
@@ -6,6 +6,7 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { toast } from 'sonner'
+import { Link } from 'react-router-dom'
 
 const signInForm = z.object({
     email: z.string().email()
@@ -43,6 +44,12 @@ export function SignIn() {
             <Helmet title='Login'/>
 
             <div className='p-8'>
+                <Button asChild variant="ghost" className="absolute right-8 top-8">  
+                    <Link to="/sign-up" >
+                        Novo estabelecimento
+                    </Link>
+                </Button>
+
                 <div className='w-[350px] flex flex-col justify-center gap-6'>
                     <div className='flex flex-col gap-2 text-center'>
                         <h1 className='text-2xl font-semibold tracking-tight'>Acessar painel</h1>
--- pizzashop-web/src/pages/routes.tsx
+++ pizzashop-web/src/pages/routes.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Dashboard } from "./app/dashboard";
 import { SignIn } from "./auth/sign-in";
+import { SignUp } from "./auth/sign-up";
 import { AppLayout } from "./_layouts/app";
 import { AuthLayout } from "./_layouts/auth";
 
@@ -18,6 +19,7 @@ export const router = createBrowserRouter([
         element: <AuthLayout/>,
         children: [
             {path: '/sign-in', element: <SignIn/>},
+            {path: '/sign-up', element: <SignUp/>},
         ]
     }
     
