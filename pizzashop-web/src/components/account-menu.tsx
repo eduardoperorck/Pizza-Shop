571babda2bb9f439858a7fc8b3f3e5da04752da7
Eduardo Ferraz <edupferraz@gmail.com>
2025-04-30 15:08:49 -0300

    feat: implement orders listing

--- pizzashop-web/src/components/account-menu.tsx
+++ pizzashop-web/src/components/account-menu.tsx
@@ -8,7 +8,7 @@ import { Skeleton } from "./ui/skeleton";
 import { Dialog, DialogTrigger } from "@radix-ui/react-dialog";
 import { StoreProfileDialog } from "./store-profile-dialog";
 import { signOut } from "@/api/sign-out";
-import { replace, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export function AccountMenu() {
 
--- pizzashop-web/src/pages/app/orders/order-table-row.tsx
+++ pizzashop-web/src/pages/app/orders/order-table-row.tsx
@@ -3,8 +3,23 @@ import { TableCell, TableRow } from "@/components/ui/table";
 import { Dialog, DialogContent, DialogTrigger } from "@radix-ui/react-dialog";
 import { ArrowRight, Search, X } from "lucide-react";
 import { OrderDetails } from "./order-details";
+import { OrderStatus } from "@/components/order-status";
 
-export function OrderTableRow() {
+import { formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+
+export interface OrderTableRowProps {
+    order: {
+        orderId: string
+        createdAt: string
+        status: 'pending' | 'canceled' | 'processing' | 'delivering' | 'delivered'
+        customerName: string
+        total: number
+    }
+}
+
+
+export function OrderTableRow({ order }: OrderTableRowProps) {
     return (
         <TableRow>
             <TableCell>
@@ -24,23 +39,26 @@ export function OrderTableRow() {
                 
             </TableCell>
             <TableCell className="font-mono text-xs font-medium">
-                454687897hjf
+                {order.orderId}
             </TableCell>
             <TableCell className="text-muted-foreground">
-                há 10 minutos
+                {formatDistanceToNow(order.createdAt, {
+                    locale: ptBR,
+                    addSuffix: true
+                })}
             </TableCell>
             <TableCell>
-                <div className="flex items-center gap-2">
-                <span className="h-2 w-2 rounded-full bg-slate-400" />
-                <span className="font-medium text-muted-foreground">
-                    Pendente
-                </span>
-                </div>
+                <OrderStatus status={order.status}/>
             </TableCell>
             <TableCell className="font-medium">
-                Eduardo Ferraz
+                {order.customerName}
             </TableCell>
-            <TableCell className="font-medium">R$ 149,90</TableCell>
+            <TableCell className="font-medium">{
+                order.total.toLocaleString('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL',
+                })}
+            </TableCell>
             <TableCell>
                 <Button variant="outline" size="xs">
                     <ArrowRight className="mr-2 h-3 w-3" />
