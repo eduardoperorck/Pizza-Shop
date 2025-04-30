1e19256888ebaafa2a2c8130fe214b21835c21cd
Eduardo Ferraz <edupferraz@gmail.com>
2025-04-30 08:42:15 -0300

    feat: add perfil in header and perfil loading state

--- pizzashop-web/src/components/account-menu.tsx
+++ pizzashop-web/src/components/account-menu.tsx
@@ -1,23 +1,49 @@
 import { Building, ChevronDown, LogOut } from "lucide-react";
 import { Button } from "./ui/button";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import { useQuery } from "@tanstack/react-query";
+import { getProfile } from "@/api/get-profile";
+import { getManagedRestaurant } from "@/api/get-manager-restaurant";
+import { Skeleton } from "./ui/skeleton";
 
 export function AccountMenu() {
+
+    const { data: profile, isLoading: isLoadingProfile } = useQuery({
+        queryKey: ['profile'],
+        queryFn: getProfile
+    })
+
+    const { data: managedRestaurant, isLoading: isLoadingManagedRestaurant } = useQuery({
+        queryKey: ['managedRestaurant'],
+        queryFn: getManagedRestaurant
+    })
+
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
                 <Button variant="outline" className="flex items-center gap-2 select-none">
-                    Pizza Shop
+                    { isLoadingManagedRestaurant ? ( <Skeleton className="h-4 w-40"/> ) : managedRestaurant?.name}
                     <ChevronDown/>
                 </Button>
             </DropdownMenuTrigger>
 
             <DropdownMenuContent align="end" className="w-56">
                 <DropdownMenuLabel className="flex flex-col">
-                    <span>Eduardo Ferraz</span>
-                    <span className="text-xs font-normal text-muted-foreground">
-                        edupferraz@gmail.com
-                    </span>
+                    {
+                        isLoadingProfile ? (
+                            <div className="space-y-1.5">
+                                <Skeleton className="h-4 w-32"/>
+                                <Skeleton className="h-4 w-24"/>
+                            </div>
+                        ) : (
+                            <>
+                                <span>{profile?.name}</span>
+                                <span className="text-xs font-normal text-muted-foreground">
+                                    {profile?.email}
+                                </span>
+                            </>
+                        )
+                    }
                 </DropdownMenuLabel>
 
                 <DropdownMenuSeparator/>
