3cf0ec1d1533a5b2ebce9006bb902f9ba477c6f1
Eduardo Ferraz <edupferraz@gmail.com>
2025-05-02 14:56:07 -0300

    test: setup MSW and create mocks for sign-up and dashboard

---
+++ pizzashop-web/src/api/mocks/index.ts
@@ -0,0 +1,30 @@
+import { setupWorker } from 'msw/browser'
+
+import { env } from '@/env'
+import { signInMock } from './sign-in-mock'
+import { registerRestaurantMock } from './register-restaurant-mock'
+import { getDayOrdersAmountMock } from './get-day-orders-amount'
+import { getMonthOrdersAmountMock } from './get-month-orders-amount'
+import { getMonthCanceledOrdersAmountMock } from './get-month-canceled-orders-amount'
+import { getMonthRevenueMock } from './get-month-revenue'
+import { getDailyRevenueInPeriodMock } from './get-daily-revenue-in-period-mock'
+import { getPopularProductsMock } from './get-popular-products-mock'
+
+export const worker = setupWorker(
+    signInMock,
+    registerRestaurantMock,
+    getDayOrdersAmountMock,
+    getMonthOrdersAmountMock,
+    getMonthCanceledOrdersAmountMock,
+    getMonthRevenueMock,
+    getDailyRevenueInPeriodMock,
+    getPopularProductsMock
+)
+
+export async function enableMSW() {
+  if (env.MODE !== 'test') {
+    return
+  }
+
+  await worker.start()
+}
