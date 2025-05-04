5e3b0560086faf32d417875934eca866784e6e38
Eduardo Ferraz <edupferraz@gmail.com>
2025-05-03 22:01:58 -0300

    test(e2e): add async assertions and end-to-end tests

--- pizzashop-web/test/sign-in.e2e-spec.ts
+++ pizzashop-web/test/sign-in.e2e-spec.ts
@@ -10,7 +10,7 @@ test('sign in successfully', async ({ page }) => {
         'Enviamos um link de autenticação para seu e-mail',
     )
 
-    expect(toast).toBeVisible()
+    await expect(toast).toBeVisible()
 })
 
 test('sign in with wrong credentials', async ({ page }) => {
@@ -19,9 +19,9 @@ test('sign in with wrong credentials', async ({ page }) => {
     await page.getByLabel('Seu e-mail').fill('wrong@example.com')
     await page.getByRole('button', { name: 'Acessar painel' }).click()
 
-    const toast = page.getByText('Credenciais inválidas.')
+    const toast = page.getByText('Credenciais inválidas')
 
-    expect(toast).toBeVisible()
+    await expect(toast).toBeVisible()
 })
 
 test('navigate to new restaurant page', async ({ page }) => {
